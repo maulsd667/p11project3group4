89b394db7723c74a94e230f29269fa806e779b5b
Julio <jcsmerubia@gmail.com>
2023-10-11 11:10:29 +0200

    js

--- Script.js
+++ Script.js
@@ -1,3 +1,18 @@
 let queryStrings = new URLSearchParams(window.location.search);
 let parametrosGet = Object.fromEntries(queryStrings.entries());
 console.log(parametrosGet);
+
+const fetchButton = document.querySelector('button.solicitar');
+const url = document.querySelector('input#link');
+const respuesta = document.querySelector('#respuestaApi');
+
+fetchButton.addEventListener('click', async () =>{
+    const apiUrl = url.value;
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        respuesta.textContent= JSON.stringify(data, null, 2);
+    }catch(error){
+        respuesta.textContent ='Error al obtener datos. Asegurate de que la URL de al API sea valida'
+    }
+})
