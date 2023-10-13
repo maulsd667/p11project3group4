096fcd85e0f766187c32d11216c34f05eb086321
Julio <jcsmerubia@gmail.com>
2023-10-13 09:42:19 +0200

    erro404

--- Script.js
+++ Script.js
@@ -6,13 +6,20 @@ const fetchButton = document.querySelector('button.solicitar');
 const url = document.querySelector('input#link');
 const respuesta = document.querySelector('#texto');
 
-fetchButton.addEventListener('click', async () =>{
+fetchButton.addEventListener('click', async () => {
     const apiUrl = url.value;
-    try{
+    try {
         const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} (${response.statusText})`);
+        }
+
         const data = await response.json();
-        respuesta.textContent= `Status Code: ${response.status}\n\n${JSON.stringify(data, null, 2)}`;
-    }catch(error){
-        respuesta.textContent ='Error al obtener datos. Asegurate de que la URL de al API sea valida'
+        respuesta.textContent = `Status Code: ${response.status}\n\n${JSON.stringify(data, null, 2)}`;
+    } catch (error) {
+        respuesta.textContent = `Status Code: ${error.message}\nError al obtener datos. Asegúrate de que la URL de la API sea válida.`;
     }
-})
+});
+
+
