d6a7dfadceae4c8801b042c6cd06486e87e5242f
isaiasd2712 <isaiasd2712@gmail.com>
2023-10-13 13:18:35 +0200

    centrado y js editado

--- Script.js
+++ Script.js
@@ -11,19 +11,19 @@ fetchButton.addEventListener('click', async () => {
     const apiUrl = url.value;
     try {
         const response = await fetch(apiUrl);
-
         if (!response.ok) {
             throw new Error(`Error: ${response.status} (${response.statusText})`);
         }
-
         const data = await response.json();
         respuesta.textContent = `Status Code: ${response.status}\n\n${JSON.stringify(data, null, 2)}`;
+        icon.src = 'iconos/bateria-cargando.png';  // Restaura la imagen A si el código es 200
+            icon.alt = 'Icono A';
     } catch (error) {
         respuesta.textContent = `Status Code: ${error.message}\n"${apiUrl}"\n 404 Error al obtener datos. Asegúrate de que la URL de la API sea válida.`;
         icon.src = 'iconos/bajo-nivel-de-bateria.png'; // Cambia la ruta a tu imagen B
         icon.alt = 'Icono B';
     }
-});
+})
 
 
 
